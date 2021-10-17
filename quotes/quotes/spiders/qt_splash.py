7df0ce29282d9f78282b746c6aeab5c2adde24c0
zloisupport <zloisupport@yandex.ru>
2021-10-17 21:28:07 +0600

    init

---
+++ quotes/quotes/spiders/qt_splash.py
@@ -0,0 +1,28 @@
+import scrapy
+from scrapy_splash import SplashRequest
+
+
+class QtSplashSpider(scrapy.Spider):
+    name = 'qt_splash'
+    allowed_domains = ['quotes.toscrape.com']
+    # start_urls = ['https://quotes.toscrape.com/js/']
+    script = '''
+    function main(splash, args)
+        url = args.url
+        assert(splash:go(url))
+        assert(splash:wait(2))
+        return splash:html()
+    end'''
+
+    def start_requests(self):
+        yield SplashRequest(url='https://quotes.toscrape.com/js/', callback=self.parse,
+                            endpoint='execute', args={'lua_source': self.script})
+
+    def parse(self, response):
+        quotes = response.xpath("//div[@class='quote']")
+        for quote in quotes:
+            yield{
+                'author': quote.xpath(".//small[@class='author']/text()").get(),
+                'text': quote.xpath(".//span[@class'text']/text()").get(),
+                'tags': quote.xpath(".//a[@class='tag']/text()").getall()
+            }
